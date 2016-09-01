3691356bef6999ee2d00efe8c386f2c1885bec60
Remember that guy with the fun and caring personality? That used to be me! <stevenramos0615@gmail.com>
2016-08-31 21:54:31 -0400

    Added scripts and made a mock up

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputManager : MonoBehaviour
+{
+    PlayerController playerController;
+    float xAxis = 0; // 1 = right, -1 = left
+    float zAxis = 0; // 1 = front, -1 back
+    float mouseXAxis = 0; // left or right movement of mouse (camera). Positive numb = right, Negative numb = left
+    float mouseYAxis = 0; // up or down movement of mouse (camera). Positive numb = up, Negative numb = down.
+
+    void Awake()
+    {
+        playerController = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<PlayerController>();
+    }
+
+    void Update()
+    {
+        mouseXAxis = Input.GetAxis("Mouse X");
+        mouseYAxis = Input.GetAxis("Mouse Y");
+        if (mouseXAxis != 0 || mouseYAxis != 0)
+        {
+            playerController.Mouselook(mouseXAxis, mouseYAxis);
+        }
+
+        xAxis = Input.GetAxisRaw("Horizontal");
+        zAxis = Input.GetAxisRaw("Vertical");
+        if (xAxis != 0 || zAxis != 0)
+        {
+            playerController.PlayerMove(xAxis, zAxis);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //playerController.PlayerJump();
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+    public float mouseSensitivity = 1;    //mouse sensitivity
+    public float playerSpeed = 1;  //We can controll the speed of the player here.
+    public Rigidbody rb;
+    public Camera cam;  //A reference to the attached camera ! be sure to actually but the camera object from the scene into the script @ the inspector window
+    
+    public void Mouselook(float mouseXAxis, float mouseYAxis)
+    {
+        // rotate the player charecter on the y axis to look left or right. This is done so that you dont fuck up the camera up
+        transform.Rotate(0, mouseXAxis * mouseSensitivity, 0);
+        // rotate the camera on the x axis to look up and down.
+        //cam.transform.Rotate(-mouseYAxis * mouseSensitivity, 0, 0);
+    }
+
+    public void PlayerMove(float xAxis, float zAxis)
+    {
+        if (xAxis != 0)
+        {
+            if (xAxis > 0)
+            {
+                //positive x = Right
+                rb.AddForce(transform.right * playerSpeed);
+            }
+
+            if (xAxis < 0)
+            {
+                rb.AddForce(-transform.right * playerSpeed);
+            }
+        }
+
+        if (zAxis != 0)
+        {
+            if (zAxis > 0)
+            {
+                //positive z = Forward
+                rb.AddForce(transform.forward * playerSpeed);
+            }
+
+            if (zAxis < 0)
+            {
+                rb.AddForce(-transform.forward * playerSpeed);
+            }
+        }
+
+    }
+
+}
