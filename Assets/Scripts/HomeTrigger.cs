1426406d53526a09ceebfd5954c336d876dd180e
Remember that guy with the fun and caring personality? That used to be me! <stevenramos0615@gmail.com>
2016-09-06 00:05:43 -0400

    FINISHED GAME, MUST EDIT AND JUICE

---
+++ Assets/Scripts/HomeTrigger.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HomeTrigger : MonoBehaviour
+{
+    public GameObject title;
+    public GameObject tut;
+
+    public GameObject hiddenClue1;
+    public GameObject hiddenClue2;
+    public GameObject hiddenClue3;
+    public GameObject hiddenClue4;
+    public GameObject hiddenClue5;
+
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.UpArrow))
+        {
+            hiddenClue4.SetActive(true);
+            hiddenClue5.SetActive(true);
+            print("Challenge1 Done");
+            Destroy(hiddenClue2);
+        }
+        if (Input.GetKeyUp(KeyCode.Delete))
+        {
+            Destroy(hiddenClue3);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        title.SetActive(false);
+        tut.SetActive(false);
+
+        hiddenClue1.SetActive(true);
+        hiddenClue2.SetActive(true);
+        hiddenClue3.SetActive(true);
+    }
+
+    void OnTriggerExit (Collider other)
+    {
+        hiddenClue1.SetActive(false);
+        hiddenClue2.SetActive(false);
+        hiddenClue3.SetActive(false);
+        hiddenClue4.SetActive(false);
+    }
+}
